741324e493b5cb9a93321c91b4e53d412bd505e7
OsmarNeto <osmar.neto23@gmail.com>
2022-08-12 01:45:16 -0300

    Finished Calculator

--- js/scripts.js
+++ js/scripts.js
@@ -3,78 +3,112 @@ class Calculator {
         this.upperValue = document.querySelector('#upper-number');
         this.resultValue = document.querySelector('#result-number');
         this.reset = 0;
+        this.ClearButtonPressed();
     }
 
-    clearValues() {
+    ClearButtonPressed() {
         this.upperValue.textContent = '0';
         this.resultValue.textContent = '0';
     }
 
-    checkLastDigit(input, upperValue, reg) {
+    CalcDisplayUpdate(total) {
+        this.upperValue.textContent = total.toString();
+        this.resultValue.textContent = total.toString();
+    }
+
+    FilterInput(input, upperValue, reg) {
         return (
             !reg.test(input) && !reg.test(upperValue.substring(upperValue.length - 1))
         );
     }
 
-    sum(n1, n2) {
+    FilterMathOperationOrder(symbol, array) {
+        if (symbol !== "x" && symbol !== "/" && symbol !== "+" && symbol !== "-") {
+            return -1;
+        } else {
+            if (symbol === "x") {
+                return 0;
+            } else if (array.includes("x")) {
+                return -1;
+            } else if (symbol === "/") {
+                return 1;
+            } else if (array.includes("/")) {
+                return -1;
+            } else if (symbol === "+") {
+                return 2;
+            } else if (array.includes("+")) {
+                return -1;
+            } else if (symbol === "-") {
+                return 3;
+            } else if (array.includes("-")) {
+                return -1;
+            }
+        }
+    }
+
+    SumOperation(n1, n2) {
         return parseFloat(n1) + parseFloat(n2);
     }
 
-    subtraction(n1, n2) {
+    SubtractionOperation(n1, n2) {
         return parseFloat(n1) - parseFloat(n2);
     }
 
-    multiplication(n1, n2) {
-        return parseFloat(n1) - parseFloat(n2);
+    MultiplicationOperation(n1, n2) {
+        return parseFloat(n1) * parseFloat(n2);
     }
 
-    division(n1, n2) {
+    DivisionOperation(n1, n2) {
         return parseFloat(n1) / parseFloat(n2);
     }
 
-
-    updateValues(total) {
-        this.upperValue.textContent = total.toString();
-        this.resultValue.textContent = total.toString();
+    ResolveMathOperation(n1, n2, operation) {
+        switch (operation) {
+            case 0:
+                return calc.MultiplicationOperation(n1, n2);
+            case 1:
+                return calc.DivisionOperation(n1, n2);
+            case 2:
+                return calc.SumOperation(n1, n2);
+            case 3:
+                return calc.SubtractionOperation(n1, n2);
+            default:
+                console.log("Operation does not exist!");
+                break;
+        }
     }
 
 
-    resolution() {
+    ResolveButtonPressed() {
         let upperValueArray = (this.upperValue.textContent).split(' ');
         let result = 0;
         for (let i = 0; i <= upperValueArray.length; i++) {
             let currentItem = upperValueArray[i];
+            let operation = 0;
 
-            let n1 = upperValueArray[i - 1];
-            let n2 = upperValueArray[i + 1];
+            let mathOperation = calc.FilterMathOperationOrder(currentItem, upperValueArray);
+            if (mathOperation !== -1) {
+                let n1 = upperValueArray[i - 1];
+                let n2 = upperValueArray[i + 1];
+                result = calc.ResolveMathOperation(n1, n2, mathOperation);
+                operation = 1;
+            }
 
-            switch (currentItem) {
-                case '+':
-                    result = calc.sum(n1, n2);
-                    break;
-                case '-':
-                    result = calc.subtraction(n1, n2);
-                    break;
-                case '/':
-                    result = calc.division(n1, n2);
-                    break;
-                case 'x':
-                    result = calc.multiplication(n1, n2);
-                    break;
-                default:
-                    continue;
+            if (operation) {
+                upperValueArray[i - 1] = result;
+                upperValueArray.splice(i, 2);
+                i = 0;
             }
 
             if (result) {
                 calc.reset = 1;
             }
 
-            calc.updateValues(result);
-
+            calc.CalcDisplayUpdate(result);
         }
     }
 
-    btnPress() {
+    CalcButtonPressed() {
         let input = this.textContent;
         let upperValue = calc.upperValue.textContent;
         let reg = new RegExp('^\\d+$');
@@ -86,11 +120,11 @@ class Calculator {
         calc.reset = 0;
 
         if (input === 'AC') {
-            calc.clearValues();
+            calc.ClearButtonPressed();
         } else if (input === '=') {
-            calc.resolution();
+            calc.ResolveButtonPressed();
         } else {
-            if (calc.checkLastDigit(input, upperValue, reg)) {
+            if (calc.FilterInput(input, upperValue, reg)) {
                 return false;
             }
 
@@ -100,7 +134,9 @@ class Calculator {
 
 
             if (upperValue === "0") {
-                calc.upperValue.textContent = input;
+                if (reg.test(input)) {
+                    calc.upperValue.textContent = input;
+                }
             } else {
                 calc.upperValue.textContent += input;
             }
@@ -110,8 +146,15 @@ class Calculator {
 }
 
 let calc = new Calculator();
-let buttons = document.querySelectorAll('.btn');
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', calc.btnPress);
+function Init() {
+    if (calc) {
+        let buttons = document.querySelectorAll('.btn');
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', calc.CalcButtonPressed);
+        }
+    }
 }
+
+Init();
+
