459cc108cbf6b11c86f13330c3f7192158871092
OsmarNeto <osmar.neto23@gmail.com>
2022-08-12 00:01:51 -0300

    Basic Math operation, 2 at a time

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,73 @@ class Calculator {
         this.reset = 0;
     }
 
+    clearValues() {
+        this.upperValue.textContent = '0';
+        this.resultValue.textContent = '0';
+    }
+
     checkLastDigit(input, upperValue, reg) {
         return (
             !reg.test(input) && !reg.test(upperValue.substring(upperValue.length - 1))
         );
+    }
+
+    sum(n1, n2) {
+        return parseFloat(n1) + parseFloat(n2);
+    }
+
+    subtraction(n1, n2) {
+        return parseFloat(n1) - parseFloat(n2);
+    }
+
+    multiplication(n1, n2) {
+        return parseFloat(n1) - parseFloat(n2);
+    }
+
+    division(n1, n2) {
+        return parseFloat(n1) / parseFloat(n2);
+    }
+
+
+    updateValues(total) {
+        this.upperValue.textContent = total.toString();
+        this.resultValue.textContent = total.toString();
+    }
+
+
+    resolution() {
+        let upperValueArray = (this.upperValue.textContent).split(' ');
+        let result = 0;
+        for (let i = 0; i <= upperValueArray.length; i++) {
+            let currentItem = upperValueArray[i];
+
+            let n1 = upperValueArray[i - 1];
+            let n2 = upperValueArray[i + 1];
+
+            switch (currentItem) {
+                case '+':
+                    result = calc.sum(n1, n2);
+                    break;
+                case '-':
+                    result = calc.subtraction(n1, n2);
+                    break;
+                case '/':
+                    result = calc.division(n1, n2);
+                    break;
+                case 'x':
+                    result = calc.multiplication(n1, n2);
+                    break;
+                default:
+                    continue;
+            }
+
+            if (result) {
+                calc.reset = 1;
+            }
+
+            calc.updateValues(result);
+
+        }
     }
 
     btnPress() {
@@ -16,14 +79,31 @@ class Calculator {
         let upperValue = calc.upperValue.textContent;
         let reg = new RegExp('^\\d+$');
 
-        if (calc.checkLastDigit(input, upperValue, reg)) {
-            return false;
+        //Reset
+        if (calc.reset && reg.test(input)) {
+            upperValue = '0';
         }
+        calc.reset = 0;
 
-        if (upperValue === "0") {
-            calc.upperValue.textContent = input;
+        if (input === 'AC') {
+            calc.clearValues();
+        } else if (input === '=') {
+            calc.resolution();
         } else {
-            calc.upperValue.textContent += input;
+            if (calc.checkLastDigit(input, upperValue, reg)) {
+                return false;
+            }
+
+            if (!reg.test(input)) {
+                input = ` ${input} `;
+            }
+
+
+            if (upperValue === "0") {
+                calc.upperValue.textContent = input;
+            } else {
+                calc.upperValue.textContent += input;
+            }
         }
 
     }
